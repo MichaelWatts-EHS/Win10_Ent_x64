e6aed061ef16beb2a1b2ec6411845eb40c9141c4
MichaelWatts-EHS <michael.watts@massmail.state.ma.us>
2018-05-22 13:10:53 -0400

    Add files via upload

--- Get-It.ps1
+++ Get-It.ps1
@@ -1,6 +1,11 @@
-﻿# Run from  an elevated PS prompt
+﻿#REQUIRES -Version 4
+#REQUIRES -RunAsAdministrator
 # Execution policy must be RemoteSigned or Unrestricted
 
+# Create the working directory
 $putPath = 'C:\MODWIM'
-If (!(Test-Path $putPath)) {New-Item -ItemType Directory -Force -Path $putPath | Out-Null}; If (!(Test-Path "$putPath\Mod-Wim.ps1")) {$client = new-object System.Net.WebClient; $client.DownloadFile('https://raw.githubusercontent.com/MichaelWatts-EHS/Win10_Ent_x64/master/Mod-Wim.ps1', "$putPath\Mod-Wim.ps1")}
+If (!(Test-Path $putPath)) {New-Item -ItemType Directory -Force -Path $putPath | Out-Null}
+
+# Get the main script and run it
+If (!(Test-Path "$putPath\Mod-Wim.ps1")) {$client = new-object System.Net.WebClient; $client.DownloadFile('https://raw.githubusercontent.com/MichaelWatts-EHS/Win10_Ent_x64/master/Mod-Wim.ps1', "$putPath\Mod-Wim.ps1")}
 If (Test-Path "$putPath\Mod-Wim.ps1") {. "$putPath\Mod-Wim.ps1"}
