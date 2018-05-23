a13d4e0d9fa0fe182084cca4ff0a1668a09f7cde
MichaelWatts-EHS <michael.watts@massmail.state.ma.us>
2018-05-22 21:01:04 -0400

    Add files via upload

--- Get-It.ps1
+++ Get-It.ps1
@@ -1,11 +1,13 @@
-﻿#REQUIRES -Version 4
+﻿# Execution policy must be RemoteSigned or Unrestricted
+
+#REQUIRES -Version 4
 #REQUIRES -RunAsAdministrator
-# Execution policy must be RemoteSigned or Unrestricted
 
-# Create the working directory
-$putPath = 'C:\MODWIM'
-If (!(Test-Path $putPath)) {New-Item -ItemType Directory -Force -Path $putPath | Out-Null}
+[CmdletBinding()]
+Param([Parameter(Mandatory=$False,Position=0)] [string] $PutPath = "$env:SystemDrive\MODWIM")
 
-# Get the main script and run it
-If (!(Test-Path "$putPath\Mod-Wim.ps1")) {$client = new-object System.Net.WebClient; $client.DownloadFile('https://raw.githubusercontent.com/MichaelWatts-EHS/Win10_Ent_x64/master/Mod-Wim.ps1', "$putPath\Mod-Wim.ps1")}
-If (Test-Path "$putPath\Mod-Wim.ps1") {. "$putPath\Mod-Wim.ps1"}
+Try {
+    If (!(Test-Path $PutPath)) {New-Item -ItemType Directory -Force -Path $PutPath | Out-Null}
+    If (!(Test-Path "$putPath\Mod-Wim.ps1")) {$client = new-object System.Net.WebClient; $client.DownloadFile('https://raw.githubusercontent.com/MichaelWatts-EHS/Win10_Ent_x64/master/Mod-Wim.ps1', "$putPath\Mod-Wim.ps1")}
+    If (Test-Path "$putPath\Mod-Wim.ps1") {. "$putPath\Mod-Wim.ps1"}
+} Catch {}
